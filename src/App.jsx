e1195b5b504e5625d7d22f68972fea8c9e0095bf
Vickkk <justusofnigeria@gmail.com>
2023-03-20 03:35:50 +0100

    dependencies added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App ">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro aperiam neque reiciendis tenetur, praesentium veritatis ratione aliquid est
+			repellendus! Exercitationem voluptatem natus iste quo inventore fugit unde atque aperiam voluptates?
+		</div>
+	);
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				main: "#49DCB1",
+			},
+			colors: {
+				main: "#49DCB1",
+			},
+		},
 	},
 	plugins: [],
 };
