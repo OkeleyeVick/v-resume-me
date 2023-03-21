b9e73acf2d499ea876a8c8de399ce262ef9f645b
Vickkk <justusofnigeria@gmail.com>
2023-03-21 15:26:42 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./assets/css/animation.css";
-import LandingPage from "./MainComponents/LandingPage";
-import ResumePage from "./MainComponents/ResumePage";
+import LandingPage from "./MainComponents/LangingPageComps/LandingPage";
+import ResumePage from "./MainComponents/ResumePageComps/ResumePage";
 
 function App() {
 	return (
 		<div className="App selection:text-green-800 selection:bg-main selection:bg-opacity-10">
 			<Routes>
 				<Route path="/" element={<LandingPage />} />
+				<Route path="/create-resume" element={<ResumePage />} />
 			</Routes>
 		</div>
 	);
---
+++ src/MainComponents/LangingPageComps/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+	return (
+		<div>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque, nisi! Quibusdam minus ratione sint in doloremque nostrum maiores?
+			Mollitia possimus nemo ab facilis laboriosam deleniti amet porro aspernatur. Dolorem, optio!
+		</div>
+	);
+};
+
+export default LandingPage;
