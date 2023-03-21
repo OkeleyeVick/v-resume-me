2f3d5184b71912fdc8d3ad632da1e1bc16a9de53
Vickkk <justusofnigeria@gmail.com>
2023-03-21 15:16:18 +0100

    pages, new component and router updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
+import { Route, Router, Routes } from "react-router-dom";
 import "./assets/css/animation.css";
+import LandingPage from "./MainComponents/LandingPage";
+import ResumePage from "./MainComponents/ResumePage";
 
 function App() {
-	// Create a new Blob object with the file content
-	// const fileContent = "This is the content of my file.";
-	// const blob = new Blob([fileContent], { type: "text/plain" });
-
-	// // Create a new File object with the Blob object and file name
-	// const fileName = "myFile.docx";
-	// const file = new File([blob], fileName, { type: "text/plain" });
-
-	// // Create a new URL object to create a download link for the file
-	// const url = URL.createObjectURL(file);
-
-	// // Create a new anchor tag with the download link and click it to download the file
-	// const downloadLink = document.createElement("a");
-	// downloadLink.href = url;
-	// downloadLink.download = fileName;
-	// downloadLink.click();
-
 	return (
 		<div className="App selection:text-green-800 selection:bg-main selection:bg-opacity-10">
-			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, neque maxime veniam est consequuntur dicta sapiente asperiores
-			mollitia incidunt, placeat sit quisquam esse consectetur molestias cum, possimus libero dolores molestiae!
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+			</Routes>
 		</div>
 	);
 }
