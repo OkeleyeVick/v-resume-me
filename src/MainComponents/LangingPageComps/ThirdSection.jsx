a5ff0eddf120d813cb27da4e6e823cf073171a86
Vickkk <justusofnigeria@gmail.com>
2023-03-23 03:16:22 +0100

    ..

--- src/MainComponents/LangingPageComps/ThirdSection.jsx
+++ src/MainComponents/LangingPageComps/ThirdSection.jsx
@@ -8,13 +8,15 @@ const ThirdSection = () => {
 		<div className="py-28">
 			<div className="grid grid-cols-2 p-4 md:px-16 gap-20">
 				<div className="col-span-full md:col-start-1 md:col-end-2 flex flex-col justify-center gap-y-8">
-					<div className="flex items-center rounded-[12px] shadow-custom_3 overflow-hidden p-3">
-						<div className=""></div>
+					<div className="flex items-center rounded-[12px] overflow-hidden p-3">
+						<div className="">
+							<h6 className="font-semibold">Download </h6>
+						</div>
 						<div className="max-w-[13rem]">
 							<img src={downloadformat} alt="" />
 						</div>
 					</div>
-					<div className="flex items-center rounded-[12px] shadow-custom_3 overflow-hidden p-3">
+					<div className="flex items-center rounded-[12px] overflow-hidden p-3">
 						<div className=""></div>
 						<div className="max-w-[13rem]">
 							<img src={documentFormat} alt="" />
