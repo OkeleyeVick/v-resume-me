9c37b8cab234155f90d09c5392ccf531686b06de
Vickkk <justusofnigeria@gmail.com>
2023-03-23 02:29:52 +0100

    updates made

--- src/MainComponents/LangingPageComps/ThirdSection.jsx
+++ src/MainComponents/LangingPageComps/ThirdSection.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import image from "../../assets/images/cv-woman.svg";
+import downloadformat from "../../assets/images/download-types.svg";
+import documentFormat from "../../assets/images/document-svg.svg";
 
 const ThirdSection = () => {
-	return <div>ThirdSection</div>;
+	return (
+		<div className="py-28">
+			<div className="grid grid-cols-2 p-4 md:px-16 gap-20">
+				<div className="col-span-full md:col-start-1 md:col-end-2 flex flex-col justify-center gap-y-8">
+					<div className="flex items-center rounded-[12px] shadow-custom_3 overflow-hidden p-3">
+						<div className=""></div>
+						<div className="max-w-[13rem]">
+							<img src={downloadformat} alt="" />
+						</div>
+					</div>
+					<div className="flex items-center rounded-[12px] shadow-custom_3 overflow-hidden p-3">
+						<div className=""></div>
+						<div className="max-w-[13rem]">
+							<img src={documentFormat} alt="" />
+						</div>
+					</div>
+				</div>
+				<div className="col-span-full md:col-start-2 md:col-end-3">
+					<img src={image} alt="" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ThirdSection;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,8 @@ module.exports = {
 			},
 			boxShadow: {
 				custom_1: "rgba(0, 0, 0, 0.24) 0px 3px 8px;",
+				custom_2: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;",
+				custom_3: "rgba(0, 0, 0, 0.16) 0px 1px 4px;",
 			},
 			animation: {
 				rotate: "rotateY 10s linear infinite",
