b516873e13636beee7bbf4769a069f69ead78ff0
Vickkk <justusofnigeria@gmail.com>
2023-03-23 02:44:38 +0100

    updates finally added for now

--- src/MainComponents/LangingPageComps/LandingPage.jsx
+++ src/MainComponents/LangingPageComps/LandingPage.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
+import PageLoader from "../PageLoader";
 import AnimatedSection from "./AnimatedSection";
 import FinalSection from "./FinalSection";
 import FirstSection from "./FirstSection";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
-import PageLoader from "./PageLoader";
 import SecondSection from "./SecondSection";
 import ThirdSection from "./ThirdSection";
 
