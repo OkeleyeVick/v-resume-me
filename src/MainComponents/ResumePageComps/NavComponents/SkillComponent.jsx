a211eae214cccc4decf1483c545ab91f20c03b8a
Vickkk <justusofnigeria@gmail.com>
2023-03-29 02:27:47 +0100

    making necessary updates

--- src/MainComponents/ResumePageComps/NavComponents/SkillComponent.jsx
+++ src/MainComponents/ResumePageComps/NavComponents/SkillComponent.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Calendar from "react-calendar";
 import Input from "../../GeneralComponents/Input";
 
 const SkillComponent = () => {
+	const [date, setDate] = useState(new Date());
+	useEffect(() => {}, [date]);
 	return (
 		<React.Fragment>
-			<Input placeholder={"Welllll"} aria-label="something else" />
+			<div>
+				<Calendar
+					onChange={() => {
+						setDate();
+
+						console.log(date);
+					}}
+					value={date}
+				/>
+				<br />
+				<br />
+				<br />
+				<br />
+			</div>
 		</React.Fragment>
 	);
 };
