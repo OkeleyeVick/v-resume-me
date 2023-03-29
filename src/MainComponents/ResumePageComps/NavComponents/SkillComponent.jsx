7e0f8bcb792eb2da7bde89222ada0e8ec7886e33
Vickkk <justusofnigeria@gmail.com>
2023-03-29 02:28:44 +0100

    updates on the next section

--- src/MainComponents/ResumePageComps/NavComponents/SkillComponent.jsx
+++ src/MainComponents/ResumePageComps/NavComponents/SkillComponent.jsx
@@ -1,28 +1,8 @@
-import React, { useEffect, useState } from "react";
-import Calendar from "react-calendar";
+import React from "react";
 import Input from "../../GeneralComponents/Input";
 
 const SkillComponent = () => {
-	const [date, setDate] = useState(new Date());
-	useEffect(() => {}, [date]);
-	return (
-		<React.Fragment>
-			<div>
-				<Calendar
-					onChange={() => {
-						setDate();
-
-						console.log(date);
-					}}
-					value={date}
-				/>
-				<br />
-				<br />
-				<br />
-				<br />
-			</div>
-		</React.Fragment>
-	);
+	return <React.Fragment></React.Fragment>;
 };
 
 export default SkillComponent;
