c290034e582f2d78a5c0f7ab24300967e0a77b4f
Vickkk <justusofnigeria@gmail.com>
2023-03-31 06:15:06 +0100

    made new updates

---
+++ src/MainComponents/GeneralComponents/GoBackButton.jsx
@@ -0,0 +1,22 @@
+import { Icon } from "@iconify/react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const GoBackButton = () => {
+	const goBack = useNavigate();
+
+	return (
+		<div
+			className="text-[.8rem] px-4 py-3 outline outline-transparent outline-1 hover:outline-main w-max rounded-md flex items-center cursor-pointer"
+			onClick={() => {
+				goBack(-1);
+			}}>
+			<button className="flex items-center gap-2 text-main">
+				<Icon icon="solar:arrow-left-outline" />
+				<span className="text-[.8rem]">Back</span>
+			</button>
+		</div>
+	);
+};
+
+export default GoBackButton;
--- src/MainComponents/ResumePageComps/NavComponents/PersonalComponent.jsx
+++ src/MainComponents/ResumePageComps/NavComponents/PersonalComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import GoBackButton from "../../GeneralComponents/GoBackButton";
 import Input from "../../GeneralComponents/Input";
 
 const PersonalComponent = () => {
 	return (
 		<React.Fragment>
-			<div className="text-end">
+			<div className="flex items-center justify-between">
+				<GoBackButton />
 				<button type="button" className="rounded-md bg-main text-slate-200 text-[.8rem] px-4 py-3 hover:bg-hoverBgClr">
 					Take hints
 				</button>
