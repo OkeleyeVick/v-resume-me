31311a1f25bf746b501ab3a393b015b3a0e7a102
Vickkk <justusofnigeria@gmail.com>
2023-04-02 01:00:53 +0100

    new month new updates

--- src/MainComponents/ResumePageComps/NavComponents/SkillComponent.jsx
+++ src/MainComponents/ResumePageComps/NavComponents/SkillComponent.jsx
@@ -31,27 +31,31 @@ const SkillComponent = () => {
 					Take hints
 				</button>
 			</div>
-			<div className="border border-main"></div>
-			<div className="flex overflow-y-auto mt-5 h-[85vh] no-scroll pr-2">
+			<div className="flex flex-col overflow-y-auto h-[85vh] no-scroll pr-2">
+				<div className="border border-main text-xs p-1 rounded-lg bg-gray-900 mt-6">
+					// This place should contain each skill and tool added with a delete button so that when done, they can paste it in the pdf format
+					or delete them before pasting them
+				</div>
 				<form action="" className="w-full mt-3">
 					<div className="flex flex-col gap-8 mt-8">
 						<h5 className="text-main text-sm font-semibold">Add skill</h5>
 					</div>
-					<div className="relative  ">
+					<div className="relative">
 						<span className="text-sm text-main font-bold">View mode</span>
 						<button
+							// disabled
 							type="button"
-							className="flex mt-2 justify-between w-full items-center rounded-md border-main border p-3"
+							className={`flex //cursor-not-allowed mt-2 justify-between w-full items-center rounded-md border-main border p-3`}
 							onClick={() => setIsActive((prev) => !prev)}>
 							<h6 className="text-sm text-gray-200">Tabs</h6>
 							<Icon icon="ph:caret-up-down-fill" className="text-main w-5 h-5" />
 						</button>
 						<div
 							className={`${
-								isActive ? "scale-y-1" : "scale-y-0"
-							} overflow-hidden absolute w-full top-full rounded-md mt-1 z-[2] p-1 right-0 left-0 h-auto border-main border bg-slate-900 origin-top`}>
+								isActive ? "h-0 opacity-0" : "opacity-100 h-[170px]"
+							} overflow-hidden absolute w-full top-full rounded-md mt-1 z-[2] right-0 left-0  border-main border bg-slate-900 origin-top`}>
 							<input type="hidden" name="" />
-							<div className="flex flex-col p-1">
+							<div className="flex flex-col  py-[.2rem]  px-[.25rem]">
 								{viewOptions.map((items, itemIndex) => {
 									const { icon, name } = items;
 									return (
