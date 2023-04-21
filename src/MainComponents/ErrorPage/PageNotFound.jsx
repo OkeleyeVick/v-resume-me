7dd7650ad0d89f4eb20187bbcc8faa816ec706cf
Vickkk <justusofnigeria@gmail.com>
2023-04-21 04:21:33 +0100

    progress

--- src/MainComponents/ErrorPage/PageNotFound.jsx
+++ src/MainComponents/ErrorPage/PageNotFound.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Footer from "../GeneralComponents/Footer";
 import Header from "../GeneralComponents/Header";
+import "../../assets/css/fonts.css";
+
+const baseFont = {
+	Syne: "Rubik",
+};
 
 const PageNotFound = () => {
 	return (
 		<React.Fragment>
-			<Header />
-			<div>PageNotFound</div>
-			<Footer />
+			<div className="min-h-screen flex flex-col justify-between" style={{ fontFamily: baseFont.Syne }}>
+				<Header />
+				<div className="text-center">
+					<h2 className="font-bold text-3xl">PageNotFound</h2>
+				</div>
+				<Footer />
+			</div>
 		</React.Fragment>
 	);
 };
