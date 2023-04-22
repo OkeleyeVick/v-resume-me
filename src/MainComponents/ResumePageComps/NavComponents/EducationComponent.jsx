9f82532905b8d0b96164b6529525b09fb347ac7c
Vickkk <justusofnigeria@gmail.com>
2023-04-22 08:24:21 +0100

    ...

--- src/MainComponents/ResumePageComps/NavComponents/EducationComponent.jsx
+++ src/MainComponents/ResumePageComps/NavComponents/EducationComponent.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import GoBackButton from "../../GeneralComponents/GoBackButton";
 
 const EducationComponent = () => {
-	return <div>EducationComponent</div>;
+	return (
+		<React.Fragment>
+			<div className="flex items-center justify-between">
+				<GoBackButton />
+				<button type="button" className="rounded-md bg-main text-slate-200 text-[.8rem] px-4 py-3 hover:bg-hoverBgClr animate-scaleInOut">
+					Take hints
+				</button>
+			</div>
+			<div className="py-3">EducationComponent</div>
+		</React.Fragment>
+	);
 };
 
 export default EducationComponent;
