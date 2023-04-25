d44bd0e03fd2fff40d4bba1473a152c1a76882a4
Vickkk <justusofnigeria@gmail.com>
2023-04-25 17:44:50 +0100

    updates again

--- src/MainComponents/ResumePageComps/TemplateDisplayBodyWrapper.jsx
+++ src/MainComponents/ResumePageComps/TemplateDisplayBodyWrapper.jsx
@@ -8,7 +8,7 @@ const TemplateDisplayBodyWrapper = ({ children }) => {
 		<React.Fragment>
 			<div className="h-full" style={{ fontFamily: font, fontSize: "10px" }}>
 				<div className="hidden sm:block p-4 xl:p-8 overflow-hidden">{children}</div>
-				<div className="sm:hidden h-full flex items-center justify-center text-gray-200 text-2xl text-center ">
+				<div className="sm:hidden pt-28 px-6 flex items-center justify-center text-gray-200 text-2xl text-center ">
 					Sorry cannot create resume on mobile phones yet, try using a larger screen device (e.g tablet, desktop, or a laptop)
 				</div>
 			</div>
