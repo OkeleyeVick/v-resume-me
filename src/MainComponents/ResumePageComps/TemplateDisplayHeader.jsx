e20e6ec3e1f51b01e16e6263a03910e13a78feec
Vickkk <justusofnigeria@gmail.com>
2023-04-27 06:51:29 +0100

    removed certain things

--- src/MainComponents/ResumePageComps/TemplateDisplayHeader.jsx
+++ src/MainComponents/ResumePageComps/TemplateDisplayHeader.jsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import Input from "../GeneralComponents/Input";
 import { MobileMenuContext } from "./CreateResumePage";
 
@@ -9,19 +9,6 @@ const TemplateDisplayHeader = () => {
 	const [formatType, setFormatType] = useState("pdf");
 	const formats = [{ format: "pdf" }, { format: "docx" }];
 	const dropdown = document.querySelector(".format__dropdown");
-
-	useEffect(() => {
-		function removeDropdown(e) {
-			if (e.currentTarget !== dropdown) {
-				setDropdownIsActive(false);
-			}
-		}
-		document.addEventListener("mousedown", removeDropdown);
-
-		return () => {
-			document.removeEventListener("mousedown", removeDropdown);
-		};
-	});
 
 	return (
 		<React.Fragment>
@@ -64,7 +51,7 @@ const TemplateDisplayHeader = () => {
 								<Icon icon="ion:chevron-down-outline" />
 							</button>
 							<div
-								className={`format__dropdown absolute border mt-1 top-full right-0 border-main rounded-md flex flex-col p-1 z-[2] bg-gray-900 w-28 px-[.2rem] ${
+								className={`absolute border mt-1 top-full right-0 border-main rounded-md flex flex-col p-1 z-[2] bg-gray-900 w-28 px-[.2rem] ${
 									dropdownActive ? "translate-y-0 opacity-100 pointer-events-auto" : "translate-y-2 opacity-0 pointer-events-none"
 								}`}>
 								<input type="hidden" name="format" value={formatType} />
