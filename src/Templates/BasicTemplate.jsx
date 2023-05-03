2e75a97b346f1bd09f5bd9b261e93fbcebe64518
Vickkk <justusofnigeria@gmail.com>
2023-05-03 06:14:35 +0100

    checking for something

--- src/Templates/BasicTemplate.jsx
+++ src/Templates/BasicTemplate.jsx
@@ -5,11 +5,11 @@ import { memo } from "react";
 
 const BasicTemplate = () => {
 	return (
-		<div className={`bg-white min-h-[100vh] p-4 grid grid-cols-3 gap-4`}>
+		<div className={`bg-white min-h-[100vh] p-4 grid grid-cols-3 gap-4 touch-pinch-zoom`}>
 			<section className="col-span-1 bg-[#DEAB52] py-4 px-3">
 				<PersonalDetails />
 			</section>
-			<section className="col-span-2 text-xs touch-pinch-zoom">
+			<section className="col-span-2 text-xs">
 				<div className="">
 					<h3 className="uppercase font-semibold text-slate-900 tracking-wider">Summary</h3>
 					<div>
